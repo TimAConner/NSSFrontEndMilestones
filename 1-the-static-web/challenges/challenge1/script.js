a3df5ed5494046e15f7490bb8a5698d688051e04
Tim A. Conner <timaconner1@gmail.com>
2017-11-14 14:46:59 -0600

    Moved challenges/ to the-static-web/.

--- 1-the-static-web/challenges/challenge1/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("rangeInput").onchange = function(){
-    document.getElementById("numberInput").value = document.getElementById("rangeInput").value;
-}; 
-document.getElementById("numberInput").onchange = function(){
-    document.getElementById("rangeInput").value = document.getElementById("numberInput").value;
-}; 
